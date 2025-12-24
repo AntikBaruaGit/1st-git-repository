5b03d4dcfd5db6a981465d19b3127725c367a3f7
AntikBaruaGit <antikbarua@outlook.com>
2025-12-24 22:26:28 +0600

    Update rockPaperScissors.js
    Added localStorage functionality to save and load game scores.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,6 +4,17 @@ let score = {
   tie: 0,
 };
 
+// load from localStorage
+
+const savedScore = localStorage.getItem("rpsScore");
+
+if (savedScore) {
+  score = JSON.parse(savedScore);
+  document.querySelector(".resultWon").innerHTML = `won: ${score.win}`;
+  document.querySelector(".resultLoss").innerHTML = `loss: ${score.loss}`;
+  document.querySelector(".resultTie").innerHTML = `tie: ${score.tie}`;
+}
+
 function playGame(playerMove) {
   let computerMove = pickcomputersMove();
   let result = "";
@@ -46,6 +57,19 @@ function playGame(playerMove) {
   document.querySelector(".resultWon").innerHTML = `won: ${score.win}`;
   document.querySelector(".resultLoss").innerHTML = `loss: ${score.loss}`;
   document.querySelector(".resultTie").innerHTML = `tie: ${score.tie}`;
+
+  /* document
+    .querySelector(".resultWon", ".resultLoss", ".resultTie")
+    .classList.add("colorRed"); */
+  // querySelector only selects one item so either write 3 lines or use foreach loop
+
+  document.querySelector(".result1").classList.add("colorYellow");
+  document
+    .querySelectorAll(".resultWon, .resultLoss, .resultTie")
+    .forEach((el) => el.classList.add("colorRed"));
+
+  // Save score
+  localStorage.setItem("rpsScore", JSON.stringify(score));
 }
 
 function pickcomputersMove() {
@@ -57,3 +81,27 @@ function pickcomputersMove() {
   else computerMove = "scissor";
   return computerMove;
 }
+
+document.querySelector(".rockImage").addEventListener("click", () => {
+  playGame("rock");
+});
+document.querySelector(".paperImage").addEventListener("click", () => {
+  playGame("paper");
+});
+document.querySelector(".scissorsImage").addEventListener("click", () => {
+  playGame("scissor");
+});
+
+document.querySelector(".resetB").addEventListener("click", () => {
+  // removing localStorage save stuff
+  localStorage.removeItem("rpsScore");
+  //updating dom
+  score = {
+    win: 0,
+    loss: 0,
+    tie: 0,
+  };
+  document.querySelector(".resultWon").innerHTML = `won: ${score.win}`;
+  document.querySelector(".resultLoss").innerHTML = `loss: ${score.loss}`;
+  document.querySelector(".resultTie").innerHTML = `tie: ${score.tie}`;
+});
